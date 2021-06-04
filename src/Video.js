5fba7fc6e48ce624946eb808d9d521ba10b76367
Rocco <roccomaniscalco2001@gmail.com>
2021-06-04 02:30:40 -0400

    formatted dateAdded with time-ago package

--- src/Video.js
+++ src/Video.js
@@ -1,13 +1,19 @@
 import { Link } from "@reach/router";
 import React from "react";
+import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+
+TimeAgo.addDefaultLocale(en);
 
 const Video = (props) => {
+  const timeAgo = new TimeAgo("en-us");
+  const dateAdded = new Date(props.dateAdded);
   return (
     <Link to={`/watch/${props.id}`} className="video-container">
       <img src={props.thumbnail.url} alt="" />
       <div className="video-info">
         <h2>{props.title}</h2>
-        <span>{props.dateAdded}</span>
+        <span>{timeAgo.format(dateAdded)}</span>
         <span>{props.channel}</span>
         <p>{props.description}</p>
       </div>
